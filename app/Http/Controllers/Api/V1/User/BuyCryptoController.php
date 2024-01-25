5c0140a028af1377fc9420183f9457c01ba0a8f4
Amir2417 <amirsohelto@gmail.com>
2024-01-25 12:22:04 +0600

    :payment pageway list api update

--- app/Http/Controllers/Api/V1/User/BuyCryptoController.php
+++ app/Http/Controllers/Api/V1/User/BuyCryptoController.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Admin\BasicSettings;
 use App\Traits\PaymentGateway\Gpay;
 use App\Http\Controllers\Controller;
-use App\Models\Admin\PaymentGateway;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 use App\Constants\PaymentGatewayConst;
@@ -73,12 +72,11 @@ class BuyCryptoController extends Controller
         });
 
         
-        // $payment_gateway    = PaymentGatewayCurrency::whereHas('gateway', function ($gateway) {
-        //     $gateway->where('slug', PaymentGatewayConst::payment_method_slug());
-        //     $gateway->where('status', 1);
-        // })->get();
-        $payment_gateway    = PaymentGateway::paymentMethod()->active()->get();
-        $payment_gateway->makeHidden(['credentials','created_at','input_fields','last_edit_by','updated_at','supported_currencies','env','slug','title','alias','code']);
+        $payment_gateway    = PaymentGatewayCurrency::whereHas('gateway', function ($gateway) {
+            $gateway->where('slug', PaymentGatewayConst::payment_method_slug());
+            $gateway->where('status', 1);
+        })->get();
+
         $image_paths = [
             'base_url'         => url("/"),
             'path_location'    => files_asset_path_basename("currency-flag"),
